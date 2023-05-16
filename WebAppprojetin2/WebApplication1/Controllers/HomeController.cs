a2edbb6bdbdb6cb4be50e403cccf954ab1d342d9
Felipe Gabriel Emídio de Freitas <e.gabriel@academico.ifrn.edu.br>
2023-05-16 15:38:15 -0300

    lista 02

---
+++ WebAppprojetin2/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            Home h = new Home();
+            h.fabi = context.Fabricantes.OrderBy(f => f.Nome);
+            h.categ = context.Categorias.OrderBy(f => f.Nome);
+            return View(h);
+        }
+    }
+}
---
+++ WebAppprojetin2/WebApplication1/Models/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Home
+    {
+        public IEnumerable<Fabricante> fabi;
+        public IEnumerable<Categoria> categ;
+    }
+}
